c5cdf01702c2df09cd095b8e794819dbe86511fb
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-07-25 13:14:55 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<String>();
+
+        for(int r = 0; r<9; r++)
+        {
+            for(int c = 0; c<9; c++)
+            {
+                char ch = board[r][c];
+                int box = (r/3) * 3 + (c/3);
+
+                if(ch != '.' && (!seen.add(ch + " in row " + r)
+                   || !seen.add(ch + " in col " + c)
+                   || !seen.add(ch + " in box " + box)))
+                {
+                    return false;
+                } 
+            }
+        }
+
+        return true;
+    }
+}
