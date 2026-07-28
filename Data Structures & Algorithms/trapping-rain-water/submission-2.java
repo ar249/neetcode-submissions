7ef787febfe93504dacf4b04eb4b0822cba84bd0
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-07-28 11:21:36 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int lm = 0;
+        int rm = 0;
+        int tw = 0;
+
+        while(l<r)
+        {
+            if(height[l] <= height[r])
+            {
+                if(height[l] >= lm)
+                {
+                    lm = height[l];
+                }
+                else
+                {
+                    tw += lm - height[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(height[r] >= rm)
+                {
+                    rm = height[r];
+                }
+                else
+                {
+                    tw += rm - height[r];
+                }
+                r--;
+            }
+        }
+
+        return tw;
+    }
+}
