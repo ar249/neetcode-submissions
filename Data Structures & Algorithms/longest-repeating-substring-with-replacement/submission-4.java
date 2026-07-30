04f40daeec8e819eb5766191e9f34ef43dd8b419
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-07-30 22:57:01 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+         int l = 0;
+         int r = 0;
+
+         int maxFre = Integer.MIN_VALUE;
+         int[] fre = new int[26];
+
+         for(int i =0; i<s.length(); i++)
+         {
+            char c = s.charAt(i);
+            int index = c - 'A';
+            fre[index]++;
+            maxFre = Math.max(maxFre, fre[index]);
+
+            int changeNeeded = r-l+1 - maxFre;
+
+            if(changeNeeded > k)
+            {
+                int idx = s.charAt(l) - 'A';
+                fre[idx]--;
+                l++;
+            }
+            r++;
+         }
+
+         return r-l;
+    }
+}
