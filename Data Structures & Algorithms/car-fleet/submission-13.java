3d39a44d4b822bb84b59947f24ecb67d04ef80cb
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-08-10 09:27:59 +0530

    Add: car-fleet - submission-13

---
+++ Data Structures & Algorithms/car-fleet/submission-13.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        double[][] cars = new double[position.length][2];
+        Stack<Double> res = new Stack<>();
+
+        for(int i = 0; i<position.length; i++)
+        {
+            cars[i][0] = position[i];
+            cars[i][1] = (double) (target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        for(int i = 0; i<cars.length; i++)
+        {
+            double time = cars[i][1];
+            if(res.isEmpty() || res.peek() < time)
+            {
+                res.push(time);
+            }
+        }
+
+        return res.size();
+    }
+}
