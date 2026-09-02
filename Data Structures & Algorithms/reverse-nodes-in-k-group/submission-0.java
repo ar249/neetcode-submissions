5273432793726b3dc2d765686de16f0082ad39f5
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-09-02 09:17:29 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head.next == null || k == 1)
+        {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prevNode = dummy;
+
+        while(true)
+        {
+            ListNode kth = getKth(prevNode, k);
+
+            if(kth == null)
+            {
+                break;
+            }
+
+            ListNode nxtNode = kth.next;
+
+            kth.next = null;
+
+            ListNode revHead = rev(prevNode);
+
+            ListNode temp = prevNode.next;
+            prevNode.next = revHead;
+            prevNode = temp;
+            prevNode.next = nxtNode;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode getKth(ListNode head, int k)
+    {
+        while(head != null && k>0)
+        {
+            head = head.next;
+            k--;
+        }
+
+        return head;
+    }
+
+    private ListNode rev(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode curr = head.next;
+
+        while(curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
