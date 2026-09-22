65a6f695ac2caa412bc5b5e4b8ed7fc7308fc204
Arindam Singh <96876969+ar249@users.noreply.github.com>
2026-09-22 09:24:56 +0530

    Add: task-scheduling - submission-3

---
+++ Data Structures & Algorithms/task-scheduling/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] fre = new int[26];
+
+        for(char c : tasks)
+        {
+            fre[c - 'A']++;
+        } 
+
+        int maxFre = 0;
+
+        for(int i : fre)
+        {
+            maxFre = Math.max(maxFre, i);
+        }
+
+        int maxFreCount = 0;
+
+        for(int i : fre)
+        {
+            if(maxFre == i)
+            {
+                maxFreCount++;
+            }
+        }
+
+        int minCycles = (n+1) * (maxFre-1) + maxFreCount;
+
+        return Math.max(tasks.length, minCycles);
+    }
+}
